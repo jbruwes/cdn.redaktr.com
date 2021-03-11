d3188c5d2c718e4f9917dda11165686e4a542704
Jerry Bruwes <jbruwes@icloud.com>
2021-03-11 16:01:11 +0300

    webpack

--- source/redaktr.js
+++ source/redaktr.js
@@ -2,6 +2,7 @@ import "lightcase";
 import "lightslider";
 import "fomantic-ui-css/semantic.js";
 import "pure";
+import "jquery-particles";
 
 import "./redaktr/rsidebar.js";
 import "./redaktr/rmenu.js";
@@ -385,14 +386,24 @@ window.jQuery = jQuery;
       .attr("contenteditable", "false")
       .each(function () {
         try {
-          tsParticles.loadJSON(
-            this.id,
-            "https://cdn.redaktr.com/resource/particles/" +
-            ($(this).data("particles")
-              ? $(this).data("particles")
-              : "default") +
-            ".json"
-          );
+
+          $(this)
+            .particles()
+            .ajax("https://cdn.redaktr.com/particles/" +
+              ($(this).data("particles")
+                ? $(this).data("particles")
+                : "default") +
+              ".json");
+          /*
+                    tsParticles.loadJSON(
+                      this.id,
+                      "https://cdn.redaktr.com/resource/particles/" +
+                      ($(this).data("particles")
+                        ? $(this).data("particles")
+                        : "default") +
+                      ".json"
+                    );
+                    */
         } catch (e) {
           console.log(e.message);
         }
